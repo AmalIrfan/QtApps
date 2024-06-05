811a9862718ac8b00b02fa0c59084bb5ba40c6bf
Amal Irfan <69129408+AmalIrfan@users.noreply.github.com>
2024-06-05 15:56:34 +0530

    Implemented plus and times
    It kinda works, not really. A lot of work need be done.

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,12 +1,77 @@
-#include <QApplication>
-#include <QPushButton>
 #include "Calculator.hpp"
 
 Calculator::Calculator(QWidget *parent)
- : QWidget(parent)
+ : QWidget(parent), registerA(0), registerB(0)
 {
- setFixedSize(200, 50);
- m_button = new QPushButton("Calculator!", this);
+ QGridLayout *mainLayout = new QGridLayout;
+
+ display = new QLineEdit("0");
+ display->setReadOnly(true);
+ display->setAlignment(Qt::AlignRight);
+ display->setMaxLength(15);
+
+ QFont font = display->font();
+ font.setPointSize(font.pointSize() + 8);
+ display->setFont(font);
+
+ mainLayout->addWidget(display, 0, 0, 1, 4);
+
+ auto createButton = [](const std::string text, auto signal, auto obj, auto slot, int row, int col, auto layout)
+ {
+  QToolButton *button = new QToolButton;
+  button->setText(text.c_str());
+  connect(button, signal, obj, slot);
+  layout->addWidget(button, row, col);
+ };
+
+ for (int i = 1; i <= 10; i++)
+ {
+  int row = (i-1) / 3 + 1;
+  int col = (i-1) % 3;
+  if (i == 10)
+   i = 0;
+ createButton(std::to_string(i), SIGNAL (clicked()), this, SLOT (digitClicked()), row, col, mainLayout);
+  if (i == 0)
+   break;
+ }
+ createButton("+", SIGNAL (clicked()), this, SLOT (plusClicked()), 1, 3, mainLayout);
+ createButton("=", SIGNAL (clicked()), this, SLOT (equalsClicked()), 4, 3, mainLayout);
+ createButton("*", SIGNAL (clicked()), this, SLOT (timesClicked()), 2, 3, mainLayout);
+ createButton("C", SIGNAL (clicked()), this, SLOT (clearClicked()), 3, 3, mainLayout);
+
+ setLayout(mainLayout);
+}
+
+void Calculator::digitClicked()
+{
+ QToolButton *button = qobject_cast<QToolButton *>(sender());
+ int digit = button->text().toInt();
+ registerB = registerB * 10 + digit;
+ display->setText(std::to_string(registerB).c_str());
+}
+void Calculator::plusClicked()
+{
+ registerA += registerB;
+ registerB = 0;
+ display->setText(std::to_string(registerB).c_str());
+}
+void Calculator::timesClicked()
+{ 
+ if (registerA == 0)
+  registerA = 1;
+ registerA *= registerB;
+ registerB = 0;
+ display->setText(std::to_string(registerB).c_str());
+}
+void Calculator::equalsClicked()
+{
+ display->setText(std::to_string(registerA).c_str());
+}
+void Calculator::clearClicked()
+{
+ registerA = 0;
+ registerB = 0;
+ display->setText(std::to_string(registerB).c_str());
 }
 
 int main(int argc, char **argv)
--- Calculator/Calculator.hpp
+++ Calculator/Calculator.hpp
@@ -1,15 +1,30 @@
 #ifndef CALCULATOR_HPP
 #define CALCULATOR_HPP
 
+#include <QApplication>
 #include <QWidget>
+#include <QToolButton>
+#include <QGridLayout>
+#include <QLineEdit>
 
-class QPushButton;
 class Calculator : public QWidget
 {
- public:
-  explicit Calculator(QWidget *parent = nullptr);
- private:
-  QPushButton *m_button;
+Q_OBJECT
+
+public:
+ Calculator(QWidget *parent = nullptr);
+
+private slots:
+ void digitClicked();
+ void plusClicked();
+ void timesClicked();
+ void equalsClicked();
+ void clearClicked();
+
+private:
+ QLineEdit *display;
+ int registerA;
+ int registerB;
 };
 
 #endif // CALCULATOR_HPP
